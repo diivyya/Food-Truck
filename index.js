f4fb0055d0f3e17766bef592198e0bae9007f8f4
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-06-03 00:33:36 +0530

    Initial Routing

--- index.js
+++ index.js
@@ -16,8 +16,15 @@ mongoose.connect('mongodb://localhost:27017/shop',{ useNewUrlParser: true})
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('WOOF!!!!!!!');
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', {products})
+})
+
+app.get('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show',{product});
 })
 
 app.listen(3000, ()=>{
